660c143fdb41a50778641856a8da0a256b5deabd
AmpinNoHito <kazuma.yamamoto819@gmail.com>
2022-07-18 08:24:43 +0900

    ディレクトリ構成を変更

--- 20220529yamamoto/rese-backend/app/Repositories/Course/CourseRepositoryInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Repositories\Course;
-
-interface CourseRepositoryInterface
-{
-    public function create($attributes);
-    public function delete($condition);
-}
--- 20220529yamamoto/rese-backend/app/Repositories/Favorite/FavoriteRepositoryInterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Repositories\Favorite;
-
-interface FavoriteRepositoryInterface
-{
-    public function create($attributes);
-    public function delete($condition);
-}
